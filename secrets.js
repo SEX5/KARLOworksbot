0cd65edc82be73972dd203c41fd2614fd3b8d101
SEX5 <64680162+SEX5@users.noreply.github.com>
2025-07-26 14:30:19 +0800

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -11,6 +11,6 @@ module.exports = {
     // --- Facebook & Google Credentials ---
     PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPJTpHW0ttQlR0w17ezQnQZA1xe0GdOJaRZAuNkL4Pz5KmHJZBr6ZB6hgm0QR8pmL493oqLsd9N0jyewWxaChbgZC9u0mtYAElrGqwWt1LmLKGVhIoeJ8USwFGq74EEoa7yGyOkJHvq0n0I7hZAAqEAhSii23UZB6YiAdjKWS2SgFDtWZAQM3x6gTdzhqSDbwQ3VesFe5Wdv3NceCwgZDZD...",
     VERIFY_TOKEN: "pinkGuineaFowl_onpella_2025", // Or your chosen token
-    ADMIN_ID: "YOUR_CORRECT_PAGE_SCOPED_ID",
-    GEMINI_API_KEY: "YOUR_GOOGLE_AI_API_KEY"
+    ADMIN_ID: "9022271741140377",
+    GEMINI_API_KEY: "AIzaSyCAIMkQPtvM8aei8QigSyBxD8WoP2hYAQI"
 };
