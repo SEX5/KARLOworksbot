6162bd452c4a6547ec46c77c5b97aabf459c2647
SEX5 <64680162+SEX5@users.noreply.github.com>
2025-08-02 11:13:48 +0800

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,3 +1,5 @@
+// --- START OF FILE secrets.js ---
+
 // secrets.js
 // ⚠️ WARNING: DO NOT UPLOAD THIS FILE TO A PUBLIC GITHUB REPO.
 // It will be added as a "Secret File" on Render.
@@ -10,5 +12,9 @@ module.exports = {
     PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPJTpHW0ttQlR0w17ezQnQZA1xe0GdOJaRZAuNkL4Pz5KmHJZBr6ZB6hgm0QR8pmL493oqLsd9N0jyewWxaChbgZC9u0mtYAElrGqwWt1LmLKGVhIoeJ8USwFGq74EEoa7yGyOkJHvq0n0I7hZAAqEAhSii23UZB6YiAdjKWS2SgFDtWZAQM3x6gTdzhqSDbwQ3VesFe5Wdv3NceCwgZDZD",
     VERIFY_TOKEN: "pinkGuineaFowl_onpella_2025",
     ADMIN_ID: "9022271741140377",
-    GEMINI_API_KEY: "AIzaSyCAIMkQPtvM8aei8QigSyBxD8WoP2hYAQI"
+    GEMINI_API_KEY: "AIzaSyCAIMkQPtvM8aei8QigSyBxD8WoP2hYAQI",
+
+    // --- Telegram Creator Bot Credentials ---
+    TELEGRAM_CREATOR_BOT_TOKEN: "7880807485:AAGvatiFWhlF_gltR1_rXaS-Hf77-sH0glM", // The token of your Python bot
+    TELEGRAM_ADMIN_CHAT_ID: "6770581734" // Your personal Telegram User ID (the admin)
 };
