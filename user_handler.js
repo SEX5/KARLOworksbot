f5d0c9d4e14716aa0035a8f3e09043b587a5ea46
SEX5 <64680162+SEX5@users.noreply.github.com>
2025-08-17 21:10:17 +0800

    Update user_handler.js

--- user_handler.js
+++ user_handler.js
@@ -139,7 +139,7 @@ Here’s what you can get right now:
 async function handleWantMod(sender_psid, text, sendText) {
     const modId = parseInt(text.replace('want mod', '').trim());
     if (isNaN(modId)) {
-        return sendText(sender_psid, `❌ Invalid format. Please type (example: 1).
+        return sendText(sender_psid, `❌ Invalid format. Please type the Number. Example: (1).
 (Type 'Menu' to return to the main menu.)`);
     }
     const mod = await db.getModById(modId);
