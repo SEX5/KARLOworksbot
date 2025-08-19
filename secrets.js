a8534d71db3c83c69508471e0c869c0faeb7d0c8
SEX5 <64680162+SEX5@users.noreply.github.com>
2025-08-19 16:47:20 +0800

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,14 +1,8 @@
 // secrets.js
-// ⚠️ WARNING: DO NOT UPLOAD THIS FILE TO A PUBLIC GITHUB REPO.
-// It will be added as a "Secret File" on Render.
+// ⚠️ IMPORTANT: DO NOT UPLOAD THIS FILE TO GITHUB.
+// Add this file to your .gitignore immediately.
 
 module.exports = {
-    // --- Neon PostgreSQL Database ---
-    NEON_DATABASE_URL: "postgresql://neondb_owner:npg_5WquzaEG4rRl@ep-noisy-glitter-a1v7gxf8-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'",
-
-    // --- Facebook & Google Credentials ---
-    PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPJTpHW0ttQlR0w17ezQnQZA1xe0GdOJaRZAuNkL4Pz5KmHJZBr6ZB6hgm0QR8pmL493oqLsd9N0jyewWxaChbgZC9u0mtYAElrGqwWt1LmLKGVhIoeJ8USwFGq74EEoa7yGyOkJHvq0n0I7hZAAqEAhSii23UZB6YiAdjKWS2SgFDtWZAQM3x6gTdzhqSDbwQ3VesFe5Wdv3NceCwgZDZD",
-    VERIFY_TOKEN: "pinkGuineaFowl_onpella_2025",
-    ADMIN_ID: "9022271741140377",
-    GEMINI_API_KEY: "AIzaSyCAIMkQPtvM8aei8QigSyBxD8WoP2hYAQI"
+    PAGE_ACCESS_TOKEN: "PASTE_YOUR_TEST_PAGE_ACCESS_TOKEN_HERE",
+    VERIFY_TOKEN: "PASTE_THE_VERIFY_TOKEN_YOU_WANT_TO_USE_HERE"
 };
