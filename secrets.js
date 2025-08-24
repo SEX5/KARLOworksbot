e388398160d6dae9c9ca41d419ac645c474b4824
SEX5 <64680162+SEX5@users.noreply.github.com>
2025-08-24 15:21:51 +0800

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     // --- Neon PostgreSQL Database ---
-    NEON_DATABASE_URL: "postgresql://neondb_owner:npg_5WquzaEG4rRl@ep-noisy-glitter-a1v7gxf8-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'",
+    NEON_DATABASE_URL: "postgresql://neondb_owner:npg_5WquzaEG4rRl@ep-noisy-glitter-a1v7gxf8-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 
     // --- Facebook & Google Credentials ---
     PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPJTpHW0ttQlR0w17ezQnQZA1xe0GdOJaRZAuNkL4Pz5KmHJZBr6ZB6hgm0QR8pmL493oqLsd9N0jyewWxaChbgZC9u0mtYAElrGqwWt1LmLKGVhIoeJ8USwFGq74EEoa7yGyOkJHvq0n0I7hZAAqEAhSii23UZB6YiAdjKWS2SgFDtWZAQM3x6gTdzhqSDbwQ3VesFe5Wdv3NceCwgZDZD",
