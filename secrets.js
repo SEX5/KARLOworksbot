20766fcd27dd359eee7a2a83dbd4526fdf1b8b69
SEX5 <64680162+SEX5@users.noreply.github.com>
2025-08-26 22:57:20 +0800

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -2,7 +2,7 @@
 module.exports = {
     // IMPORTANT: Use the "Transaction Pooler" URL from Supabase.
     // It must end with port :6543
-    DATABASE_URL: "postgresql://postgres:abalunan1@db.vburnklpeuvxswtrugbp.supabase.co:5432/postgres",
+    DATABASE_URL: "postgresql://postgres.vburnklpeuvxswtrugbp:abalunan1@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres",
     // --- Other secrets ---
     PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPJTpHW0ttQlR0w17ezQnQZA1xe0GdOJaRZAuNkL4Pz5KmHJZBr6ZB6hgm0QR8pmL493oqLsd9N0jyewWxaChbgZC9u0mtYAElrGqwWt1LmLKGVhIoeJ8USwFGq74EEoa7yGyOkJHvq0n0I7hZAAqEAhSii23UZB6YiAdjKWS2SgFDtWZAQM3x6gTdzhqSDbwQ3VesFe5Wdv3NceCwgZDZD",
     VERIFY_TOKEN: "pinkGuineaFowl_onpella_2025",
