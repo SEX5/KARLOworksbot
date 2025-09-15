0fd6854f66c9ee61b1ac8985badf704221430e30
iamkarloone-ux <iamkarloone@gmail.com>
2025-09-15 12:45:08 +0800

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -4,7 +4,7 @@ module.exports = {
     // It must end with port :6543
     DATABASE_URL: "postgresql://postgres.vburnklpeuvxswtrugbp:abalunan1@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres",
     // --- Other secrets ---
-    PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPJTpHW0ttQlR0w17ezQnQZA1xe0GdOJaRZAuNkL4Pz5KmHJZBr6ZB6hgm0QR8pmL493oqLsd9N0jyewWxaChbgZC9u0mtYAElrGqwWt1LmLKGVhIoeJ8USwFGq74EEoa7yGyOkJHvq0n0I7hZAAqEAhSii23UZB6YiAdjKWS2SgFDtWZAQM3x6gTdzhqSDbwQ3VesFe5Wdv3NceCwgZDZD",
+    PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPcqZBX0DqC87rsMkZCGfshesgIQAgVcujp3G286CMR9uETlp048DFNWXLnKFRkCMkpD0QRwevWxy4c8Og9yJjNqJhyIl5G4XmSTFXYCGGpVhu1XnwZAZCzUxTZCP4JgNRSDYumZCNRxyIYMoBINxWzzZCERwmx3Vzoa8lQ0F3X62T0RLmlo5v7ZCpjsm6T561CDIGZBbNT1DHvZAiKrgZDZD",
     VERIFY_TOKEN: "pinkGuineaFowl_onpella_2025",
     ADMIN_ID: "9022271741140377",
     GEMINI_API_KEY: "AIzaSyCAIMkQPtvM8aei8QigSyBxD8WoP2hYAQI"
