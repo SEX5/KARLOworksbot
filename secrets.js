8f9b651e52c186a26875439e7323b65f503ccd69
SEX5 <64680162+SEX5@users.noreply.github.com>
2025-09-21 10:52:24 +0800

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -7,5 +7,9 @@ module.exports = {
     PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPcqXg2AY2y0reyZAOnRj2GIveLqop4ibLMoTQZBNo7w2fkLaO6ZCvssgfhOsndEPiyZCv59SguqWt7bARCBtLBrZBB9Mbjkem5ZAtK06u9qTNv8Mj6z1fhZBMN3O7SWBwZACSEJjVTNCYxiat0tp7HChOPvrIhagCwX0j4faZBiZA3zthfrGd0C7iMRTMvQRoe1GAuSJJIWTwRkn9FdwZDZD",
     VERIFY_TOKEN: "pinkGuineaFowl_onpella_2025",
     ADMIN_ID: "9022271741140377",
-    GEMINI_API_KEY: "AIzaSyCAIMkQPtvM8aei8QigSyBxD8WoP2hYAQI"
-};
+    GEMINI_API_KEY: "AIzaSyCAIMkQPtvM8aei8QigSyBxD8WoP2hYAQI", 
+    BOT_URL: "https://apiworker.onrender.com", 
+
+    // A secure, random password that the Python worker uses to talk to your bot.
+    WORKER_SECRET_TOKEN: "CREATE_A_LONG_AND_SECRET_WORKER_PASSWORD_HERE" 
+}; 
