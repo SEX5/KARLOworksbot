2989183331782077babc7038ae2e8a8a41d1cb2f
iamkarloone-ux <iamkarloone@gmail.com>
2025-10-15 07:33:15 +0800

    Update menu_handler.js

--- user_handler/menu_handler.js
+++ user_handler/menu_handler.js
@@ -3,13 +3,12 @@ const db = require('../database');
 const messengerApi = require('../messenger_api.js');
 const lang = require('../language_manager');
 
-// CORRECTED: The function no longer needs sendQuickReplies passed to it.
 async function showUserMenu(sender_psid, userLang = 'en') {
     const adminInfo = await db.getAdminInfo();
     let initialMessage = adminInfo?.is_online ? lang.getText('admin_online', userLang) : lang.getText('admin_offline', userLang);
     await messengerApi.sendText(sender_psid, initialMessage);
 
-    const menuText = `${lang.getText('welcome_message', userLang)}\n\n${lang.getText('menu_option_1', userLang)}\n${lang.getText('menu_option_2', userLang)}\n${lang.getText('menu_option_3', userLang)}\n${lang.getText('menu_option_4', userLang)}\n${lang.getText('menu_option_5', userLang)}\n${lang.getText('menu_option_6', userLang)}\n${lang.getText('menu_option_7', userLang)}\n\n${lang.getText('menu_suffix', userLang)}`;
+    const menuText = `${lang.getText('welcome_message', userLang)}\n\n${lang.getText('menu_option_1', userLang)}\n${lang.getText('menu_option_2', userLang)}\n${lang.getText('menu_option_3', userLang)}\n${lang.getText('menu_option_4', userLang)}\n${lang.getText('menu_option_5', userLang)}\n${lang.getText('menu_option_6', userLang)}\n${lang.getText('menu_option_7', userLang)}\n${lang.getText('menu_option_8', userLang)}\n\n${lang.getText('menu_suffix', userLang)}`;
     
     const replies = [
         { title: lang.getText('menu_option_1_button', userLang), payload: "1" },
@@ -19,9 +18,9 @@ async function showUserMenu(sender_psid, userLang = 'en') {
         { title: lang.getText('menu_option_5_button', userLang), payload: "5" },
         { title: lang.getText('menu_option_6_button', userLang), payload: "6" },
         { title: lang.getText('menu_option_7_button', userLang), payload: "7" },
+        { title: lang.getText('menu_option_8_button', userLang), payload: "8" },
     ];
     
-    // CORRECTED: This now uses the imported messengerApi, just like sendText does.
     await messengerApi.sendQuickReplies(sender_psid, menuText, replies);
 }
 
