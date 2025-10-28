16d39e3eac38bad736a7b29b87b0977aec818ad0
iamkarloone-ux <iamkarloone@gmail.com>
2025-10-28 14:03:18 +0800

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -7,7 +7,7 @@ module.exports = {
     PAGE_ACCESS_TOKEN: "EAA5a1El5oeIBPcqXg2AY2y0reyZAOnRj2GIveLqop4ibLMoTQZBNo7w2fkLaO6ZCvssgfhOsndEPiyZCv59SguqWt7bARCBtLBrZBB9Mbjkem5ZAtK06u9qTNv8Mj6z1fhZBMN3O7SWBwZACSEJjVTNCYxiat0tp7HChOPvrIhagCwX0j4faZBiZA3zthfrGd0C7iMRTMvQRoe1GAuSJJIWTwRkn9FdwZDZD",
     VERIFY_TOKEN: "pinkGuineaFowl_onpella_2025",
     ADMIN_ID: "9022271741140377",
-    GEMINI_API_KEY: "AIzaSyCAIMkQPtvM8aei8QigSyBxD8WoP2hYAQI",
+    GEMINI_API_KEY: "AIzaSyAaF1PIIgkph1RqKdqN7xH1HyFXwXOEULc",
     KAIZ_API_KEY: "732ce71f-4761-474d-adf2-5cd2d315ad18", // Your new Kaiz-APIs key
     BOT_URL: "https://apiworker.onrender.com", 
 
